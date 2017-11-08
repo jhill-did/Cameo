0948f22ae3a6ad7719c8d4a5c9cc26cf37b09974
Jordan Hill <puddin482@yahoo.com>
2017-11-08 01:56:47 -0600

    Fixed up line number scrolling.
    Better but still not perfect. Probably good for a longer term TODO.

--- src/Cameo/CameoMenuBar.java
+++ src/Cameo/CameoMenuBar.java
@@ -1,13 +1,15 @@
 package Cameo;
 import java.io.File;
 
-import javax.swing.event.HyperlinkEvent.EventType;
+import javax.swing.JPopupMenu.Separator;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.*;
+import javafx.scene.input.KeyCombination;
 import javafx.scene.*;
 import javafx.stage.*;
+import javafx.scene.input.*;
 
 public class CameoMenuBar extends MenuBar 
 {
@@ -15,7 +17,14 @@ public class CameoMenuBar extends MenuBar
 	{
 		Menu file = new Menu("File");
 		MenuItem newFile = new MenuItem("New File");
+		newFile.setAccelerator(new KeyCodeCombination(KeyCode.N, KeyCombination.CONTROL_DOWN));
 		MenuItem openFile = new MenuItem("Open File");
+		openFile.setAccelerator(new KeyCodeCombination(KeyCode.O, KeyCombination.CONTROL_DOWN));
+		
+		SeparatorMenuItem separator = new SeparatorMenuItem();
+		
+		MenuItem closeTab = new MenuItem("Close Tab");
+		closeTab.setAccelerator(new KeyCodeCombination(KeyCode.W, KeyCombination.CONTROL_DOWN));
 		
 		openFile.setOnAction(new EventHandler<ActionEvent>()
 		{
@@ -36,7 +45,7 @@ public class CameoMenuBar extends MenuBar
 		MenuItem undo = new MenuItem("Undo");
 		MenuItem redo = new MenuItem("Redo");
 
-		file.getItems().addAll(newFile, openFile);
+		file.getItems().addAll(newFile, openFile, separator, closeTab);
 		getMenus().add(file);
 		edit.getItems().addAll(undo, redo);
 		getMenus().add(edit);
--- src/Cameo/DocumentTab.java
+++ src/Cameo/DocumentTab.java
@@ -4,8 +4,19 @@ import org.omg.CosNaming.NamingContextExtPackage.AddressHelper;
 import Cameo.Model.Document;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
 import javafx.scene.control.*;
+import javafx.scene.control.ScrollPane.ScrollBarPolicy;
 import javafx.scene.layout.*;
+import javafx.scene.text.TextAlignment;
+import javafx.scene.text.*;
+
+// | container : border setup placing lineNumbers on the left and textArea in the center. 
+//	 | scrollPane : Scrolls the innerContainer
+//	 	| lineNumbers : displays line numbers using Labels by counting up textArea linebreaks.
+//	 | textArea : displays document text.
 
 public class DocumentTab extends Tab
 {
@@ -16,6 +27,8 @@ public class DocumentTab extends Tab
 	public DocumentTab(Document document)
 	{
 		this.document = document;
+		
+		// Set tab title
 		setText(document.filename);
 		
 		textArea.textProperty().addListener(new ChangeListener<String>()
@@ -23,31 +36,62 @@ public class DocumentTab extends Tab
 			@Override
 			public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue)
 			{
-				// TODO: This is jacked up but kind of the right idea.
 				int newLineCount = newValue.length() - newValue.replace("\n", "").length();
 				while (lineNumbers.getChildren().size() - 1 != newLineCount)
 				{
 					int currentLineCount = lineNumbers.getChildren().size() - 1;
 					if (currentLineCount < newLineCount)
 					{
-						lineNumbers.getChildren().add(new Label(currentLineCount + 2 + ""));
+						Label temp = new Label(currentLineCount + 2 + "");
+						lineNumbers.getChildren().add(temp);
 					}
 					else
 					{
 						lineNumbers.getChildren().remove(currentLineCount - 1);
 					}
 				}
+				
+				document.content = newValue;
 			}
 		});
 
 		textArea.setText(document.content);
+		textArea.getStylesheets().add("style/style.css");
+		textArea.applyCss();
 
+		double lineNumbersTopPadding = 4;
+		lineNumbers.setStyle("-fx-padding: " + lineNumbersTopPadding + " 5 0 5");
+		lineNumbers.setAlignment(Pos.CENTER_RIGHT);
+		
+		ScrollBar sizeTest = new ScrollBar();
+		sizeTest.setOrientation(Orientation.HORIZONTAL);
+		
+		ScrollPane scrollPane = new ScrollPane();
+		scrollPane.setContent(lineNumbers);
+		scrollPane.setHbarPolicy(ScrollBarPolicy.NEVER);
+		scrollPane.setVbarPolicy(ScrollBarPolicy.NEVER);
+		scrollPane.setFitToWidth(true);
+		scrollPane.setFitToHeight(true);
+		
+		// Mirror lineNumber's scroll pane with the text area scroll.
+		textArea.scrollTopProperty().addListener(new ChangeListener<Number>()
+		{
+			@Override
+			public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue)
+			{
+				Text textContent = (Text)textArea.lookup(".text");
+				scrollPane.setVmin(0);
+				scrollPane.setVmax(1 + ((lineNumbersTopPadding * 2) / lineNumbers.getHeight()));
+				
+				scrollPane.setVvalue((double)newValue / (textContent.getBoundsInLocal().getHeight() - textArea.getHeight()));
+			}
+		});
+		
 		BorderPane container = new BorderPane();
-		container.setLeft(lineNumbers);
+		container.setLeft(scrollPane);
 		container.setCenter(textArea);
 		
 		setContent(container);
-		
 	}
 	
 	public String getDocumentId()
